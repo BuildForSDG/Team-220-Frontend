706c80a7f69be5a2632da6c060d257f2b5530f55
Willy Nzesseu <willynzesseu@gmail.com>
2020-05-10 01:29:26 +0000

    feat: Add linting, code formatting and testing setup to the repo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-    jest: true
-  },
-  extends: ['airbnb-base'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  rules: {
-    'comma-dangle': ['error', 'never'],
-    'no-param-reassign': ['error', { props: false }]
-  }
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+		jest: true
+	},
+	extends: [
+		'airbnb-base',
+		'airbnb-angular',
+		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+		'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+		'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+	],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly'
+	},
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module'
+	},
+	rules: {
+		'comma-dangle': ['error', 'never'],
+		'no-param-reassign': ['error', { props: false }]
+	}
 };
