bf041a927e460e8f6976648fe49ae4e5d688d084
Willy Nzesseu <willynzesseu@gmail.com>
2020-05-15 05:01:15 +0000

    feat: add testimonials to landing

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -4,9 +4,10 @@ import { LandingComponent } from './landing.component';
 import { OurServicesComponent } from './OurServices/ourservices.component';
 import { CtaComponent } from './cta/cta.component';
 import { SharedModule } from '../shared/shared.module';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
-	declarations: [LandingComponent, OurServicesComponent, CtaComponent],
+	declarations: [LandingComponent, OurServicesComponent, CtaComponent, TestimonialsComponent],
 	imports: [CommonModule, SharedModule]
 })
 export class LandingModule {}
